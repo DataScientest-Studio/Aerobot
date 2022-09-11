d86e3b418102ece176dbe24431401642de073461
ioannis STASINOPOULOS <97918270+Cochonaki@users.noreply.github.com>
2022-09-11 12:43:43 +0300

    Update graphics.py

--- packages/graphics.py
+++ packages/graphics.py
@@ -2,21 +2,20 @@
 ######################
 # PLOTTING
 ######################
-import numpy as np
 import matplotlib.pyplot as plt
-#%matplotlib inline
-# Define global plot parameters for better readability and consistency among plots
-# A complete list of the rcParams keys can be retrieved via plt.rcParams.keys() function
-plt.rcParams['axes.titlesize'] = 30
-plt.rcParams['axes.labelsize'] = 23
-plt.rcParams['xtick.labelsize'] = 23
-plt.rcParams['ytick.labelsize'] = 23
-plt.rc('legend', fontsize=23)    # legend fontsize
+# #%matplotlib inline
+# # Define global plot parameters for better readability and consistency among plots
+# # A complete list of the rcParams keys can be retrieved via plt.rcParams.keys() function
+# plt.rcParams['axes.titlesize'] = 30
+# plt.rcParams['axes.labelsize'] = 23
+# plt.rcParams['xtick.labelsize'] = 23
+# plt.rcParams['ytick.labelsize'] = 23
+# plt.rc('legend', fontsize=23)    # legend fontsize
 
-# BOKEH 
-from bokeh.plotting import figure # Importation de la classe figure qui permet de créer un graphique bokeh.
-from bokeh.io import  push_notebook, output_notebook, show
-output_notebook() # permet d'afficher tous les futurs graphiques dans l'output d'une cellule jupyter. Si cette instruction n'est pas lancée, la figure s'affichera dans un nouvel onglet.
-from bokeh.models import ColumnDataSource
-from bokeh.transform import dodge
-from bokeh.models.tools import HoverTool
+# # BOKEH 
+# from bokeh.plotting import figure # Importation de la classe figure qui permet de créer un graphique bokeh.
+# from bokeh.io import  push_notebook, output_notebook, show
+# output_notebook() # permet d'afficher tous les futurs graphiques dans l'output d'une cellule jupyter. Si cette instruction n'est pas lancée, la figure s'affichera dans un nouvel onglet.
+# from bokeh.models import ColumnDataSource
+# from bokeh.transform import dodge
+# from bokeh.models.tools import HoverTool
