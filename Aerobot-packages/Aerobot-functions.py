0309a8cf4757d960a13d725f25b9d7a208d27686
Cochonaki <97918270+Cochonaki@users.noreply.github.com>
2022-09-11 13:20:24 +0300

    Update packages

--- Aerobot-packages/Aerobot-functions.py
+++ Aerobot-packages/Aerobot-functions.py
@@ -1,25 +1,5 @@
 
-######################
-# PLOTTING
-######################
-def my_funct():
-  print('I am a custom-defined function')
-  
+# project-specific functions created by the 'Aerobot' project members
 
-# import matplotlib.pyplot as plt
-# #%matplotlib inline
-# # Define global plot parameters for better readability and consistency among plots
-# # A complete list of the rcParams keys can be retrieved via plt.rcParams.keys() function
-# plt.rcParams['axes.titlesize'] = 30
-# plt.rcParams['axes.labelsize'] = 23
-# plt.rcParams['xtick.labelsize'] = 23
-# plt.rcParams['ytick.labelsize'] = 23
-# plt.rc('legend', fontsize=23)    # legend fontsize
-
-# # BOKEH 
-# from bokeh.plotting import figure # Importation de la classe figure qui permet de créer un graphique bokeh.
-# from bokeh.io import  push_notebook, output_notebook, show
-# output_notebook() # permet d'afficher tous les futurs graphiques dans l'output d'une cellule jupyter. Si cette instruction n'est pas lancée, la figure s'affichera dans un nouvel onglet.
-# from bokeh.models import ColumnDataSource
-# from bokeh.transform import dodge
-# from bokeh.models.tools import HoverTool
+def Aerobot_funct():
+  print('I am a custom-defined function defined in the project Aerobot')
