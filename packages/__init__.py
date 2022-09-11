f277ef0d8466ef7a888d58ff163124aa7241a066
ioannis STASINOPOULOS <97918270+Cochonaki@users.noreply.github.com>
2022-09-11 12:32:58 +0300

    Update __init__.py

--- packages/__init__.py
+++ packages/__init__.py
@@ -1,1 +1,9 @@
-
+#-----------------------------------------------------------
+# PROJET AeroBOT
+# Datascientest DS continu mars. 2022
+#
+# Authors : Ioannis STASINOPOULOS
+# Hichem HAJJI
+# Helene ASSIR
+#-----------------------------------------------------------\
+from .graphics import *
