f208f19ee0f2ce5d6e4dc916946a79bece5433aa
ioannis STASINOPOULOS <97918270+Cochonaki@users.noreply.github.com>
2022-10-23 13:46:41 +0200

    Update BERTTools.py
    Debug numpy import

--- aerobotpackages/BERTTools.py
+++ aerobotpackages/BERTTools.py
@@ -1,8 +1,109 @@
 # project-specific functions and classes created in the 'Aerobot' project by Ioannis STASINOPOULOS
 # This file contains functions and classes for the BERT implementation
 
+#######################
+# Import packages
+#######################
 import numpy as np
+import seaborn as sns
+import math # for math.pi etc.
+import time # time code execution
 
+#######################
+# Pandas
+#######################
+import pandas as pd
+# Set pandas settings to show all data when using .head(), .columns etc.
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+pd.set_option("display.colheader_justify","left") # left-justify the print output of pandas
+
+### Display full columnwidth
+# Set pandas settings to display full text columns
+#pd.options.display.max_colwidth = None
+# Restore pandas settings to display standard colwidth
+pd.reset_option('display.max_colwidth')
+
+import itertools # Pour créer des iterateurs
+
+# Package to show the progression of pandas operations
+from tqdm import tqdm
+# from tqdm.auto import tqdm  # for notebooks
+
+# Create new `pandas` methods which use `tqdm` progress
+# (can use tqdm_gui, optional kwargs, etc.)
+tqdm.pandas()
+# simply use .progress_apply() instead of .apply() on your pd.DataFram
+
+######################
+# PLOTTING
+######################
+import matplotlib.pyplot as plt
+%matplotlib inline
+# Define global plot parameters for better readability and consistency among plots
+# A complete list of the rcParams keys can be retrieved via plt.rcParams.keys() function
+plt.rcParams['axes.titlesize'] = 30
+plt.rcParams['axes.labelsize'] = 23
+plt.rcParams['xtick.labelsize'] = 23
+plt.rcParams['ytick.labelsize'] = 23
+plt.rc('legend', fontsize=23)    # legend fontsize
+
+# BOKEH 
+from bokeh.plotting import figure # Importation de la classe figure qui permet de créer un graphique bokeh.
+from bokeh.io import  push_notebook, output_notebook, show
+output_notebook() # permet d'afficher tous les futurs graphiques dans l'output d'une cellule jupyter. Si cette instruction n'est pas lancée, la figure s'affichera dans un nouvel onglet.
+from bokeh.models import ColumnDataSource
+from bokeh.transform import dodge
+from bokeh.models.tools import HoverTool
+
+#####################
+# NLP 
+#####################
+import re # for Regular Expression handling
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet') # WordNet lemmatizer
+nltk.download('omw-1.4') # necessary for WordNet lemmatizer
+from nltk.tokenize import word_tokenize # Usual tokenizer
+from nltk.tokenize import TweetTokenizer # Special tokenizer;  "we'll", "didn't", etc. are considered as one word
+from sklearn.feature_extraction.text import CountVectorizer # Vectorization
+from nltk.corpus import stopwords # Import stopwords from nltk.corpus
+from nltk.stem.snowball import EnglishStemmer
+
+###############################
+# ML preprocessing and models
+###############################
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import ensemble # random forest
+from sklearn.svm import SVC
+
+# EVALUATION tools from sklearn
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import roc_curve, auc, multilabel_confusion_matrix, average_precision_score, precision_recall_curve, PrecisionRecallDisplay
+
+###############################
+# Deep Learning
+###############################
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Layer, Input, Dense, Embedding, Flatten, Dropout, GlobalAveragePooling1D
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.losses import sparse_categorical_crossentropy
+from tensorflow.keras import callbacks
+
+
+###############################
+# Other
+###############################
+import pickle as pkl # Saving data externally
+
+###########################################
 class DataPrepMultilabelBERT():
     '''
     Prepare data for multilabel classification using BERT.
