92a6c21170bbfc568e4b989cce40e8fe88879ea4
ioannis STASINOPOULOS <97918270+Cochonaki@users.noreply.github.com>
2022-10-23 13:21:23 +0200

    Update __init__.py to include BERTTools

--- aerobotpackages/__init__.py
+++ aerobotpackages/__init__.py
@@ -2,10 +2,10 @@
 # PROJET AeroBOT
 # Datascientest DS continu mars. 2022
 #
-# Authors : Ioannis STASINOPOULOS
-# Hichem HAJJI
-# Helene ASSIR
+# Authors : 
+# Ioannis STASINOPOULOS
 #-----------------------------------------------------------\
 from .aerobotfunctions import *
 from .aerobotgraphics import *
+from .BERTTools import *
 # include additional package names here
