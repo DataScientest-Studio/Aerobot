a69648c212bce221d68572419c1a233be0eb0831
ioannis STASINOPOULOS <97918270+Cochonaki@users.noreply.github.com>
2022-11-01 12:31:37 +0100

    Delete aerobotfunctions.py
    unused

--- aerobotpackages/aerobotfunctions.py
+++
@@ -1,6 +0,0 @@
-# project-specific functions created by the 'Aerobot' project members
-# This is a dummy module; define additional modules with more meaningful names 
-# in order to better organize your custom functions, classes etc.
-
-def Aerobot_funct():
-  print('I am a custom-defined function defined in the project Aerobot')
