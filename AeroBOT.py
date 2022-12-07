916a7f4309ff184e8bae063d931b2f4cd749ecdb
Cochonaki <97918270+Cochonaki@users.noreply.github.com>
2022-12-07 16:00:39 +0200

    Update requirements.txt and AeroBOT.py
    - requirements.txt : add packages for streamlit
    - AeroBOT.py work with pathlib and download automatically files for streamlit

--- AeroBOT.py
+++ AeroBOT.py
@@ -8,6 +8,8 @@ import gdown
 import zipfile
 import argparse
 import pickle as pkl
+import pathlib
+from pathlib import Path  
 
 # Custom-made, project-specific packages
 from aerobotpackages import train_load_transformer_model
@@ -16,41 +18,57 @@ from aerobotpackages import train_load_transformer_model
 # DOWNLOAD FILES
 ##############################################
 print("Checking the presence of necessary data for inference, download if necessary.")
-if not os.path.exists('./data/transformed/df_test_for_Anomaly_prediction.pkl'):
-    # Download final test set data
-    os.chdir("./data/transformed/") 
-    url = 'https://drive.google.com/uc?id=1HZSxIfwGqg38yByiXxS4EjyLgeHI6yOv'
-    output = 'df_test_for_Anomaly_prediction.pkl'
-    print('Downloading final test set data...')
-    gdown.download(url, output, quiet=False)
-    # Move up 2 directories, to the location of the present script
-    os.chdir('..')
-    os.chdir('..')
+AeroBOT_root_dir = Path.cwd()
+trans_data_path = AeroBOT_root_dir.joinpath('data', 'transformed')
+model_data_path = AeroBOT_root_dir.joinpath('data', 'models')
 
-else: 
-    print('df_test_for_Anomaly_prediction.pkl is already present.')
+def download_from_GDrive(path, filename, url, zipped=False):
+    """
+    Inputs
+    -------
+    - path: path where to look for the file and if not present, download. Use OS-independent paths with the help of the pathlib library
+    - file_to_download (str): filename of the file to download
+    - url (str): Google Drive URL of the file to download. Example: 'https://drive.google.com/uc?id=1HZSxIfwGqg38yByiXxS4EjyLgeHI6yOv'
+    
+    If filename is .zip, will unzip it into a folder named [filename].
 
-if not os.path.exists('./data/models/11_3_3/'):
-    # Download transformer model
-    os.chdir("./data/models/") 
-    url = 'https://drive.google.com/uc?id=1qUGjWwJ9vLutgw_P-1Ax2Hl3DSm-7XGX'
-    output = 'model_11_3_3.zip'
-    print('Downloading transformer model zip file...')
-    gdown.download(url, output, quiet=False)
+    Return
+    -------
+    None
+    """
+    if path.joinpath('11_3_3').exists(): # the model has been already unzipped
+        print('check this:', path.joinpath('11_3_3').exists())
+        return None 
 
-    with zipfile.ZipFile('model_11_3_3.zip', 'r') as zip_ref:
-        print('Unzipping the model file...')
-        zip_ref.extractall('') # if '', extracts into pwd
+    if not path.joinpath(filename).exists(): # no files are present
+        os.chdir(path) 
+        print("Downloading file '{}'...".format(filename))
+        gdown.download(url, filename, quiet=False)
 
-    print("Deleting zip file...")
-    os.remove('model_11_3_3.zip')
-    print("Zip file successfully deleted.")
+    else:
+        print("File '{}' is already present.".format(filename))
+        if filename[-4:] == '.zip':
+            os.chdir(model_data_path) 
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                print('Unzipping the file...')
+                zip_ref.extractall('') # if '', extracts into pwd     
+            print("Deleting zip file...")
+            os.remove(filename)
+            print("Zip file was deleted successfully.")
+    os.chdir(AeroBOT_root_dir)
+    
+    return None
 
-    # Move up 2 directories, to the location of the present script
-    os.chdir('..')
-    os.chdir('..')
-else: 
-    print('Transformer model 11_3_3 is already present.')
+download_from_GDrive(trans_data_path, 
+                    filename = 'df_test_for_Anomaly_prediction.pkl', 
+                    url = 'https://drive.google.com/uc?id=1HZSxIfwGqg38yByiXxS4EjyLgeHI6yOv')
+download_from_GDrive(model_data_path, 
+                    filename = 'model_11_3_3.zip', 
+                    url = 'https://drive.google.com/uc?id=1qUGjWwJ9vLutgw_P-1Ax2Hl3DSm-7XGX')
+# Files for streamlit demo
+download_from_GDrive(trans_data_path, 
+                    filename = 'model_results_diffBLM_bestmodel_20221207.csv', 
+                    url = 'https://drive.google.com/uc?id=1Fv__EH0Z6gUSirpKzEmrDUsKkbBtxMEb')                    
 
 ##############################################
 # Manage the arguments passed in the cmd line
@@ -67,13 +85,13 @@ args = parser.parse_args()
 ##############################################
 # Load the FINAL TEST data (10805 entries)
 ##############################################
-os.chdir("./data/transformed/") 
+os.chdir(trans_data_path)  
 print("Loading pd.DataFrame...")
 with open("df_test_for_Anomaly_prediction.pkl", "rb") as f:
     loaded_data = pkl.load(f)
 
 df = loaded_data
-os.chdir("..")
+os.chdir(AeroBOT_root_dir)
 print("A Dataframe with", len(df), "entries has been loaded.")
 print(f'Only the {args.limit} first entries will be used.')
 
@@ -88,14 +106,11 @@ for col in df.columns:
 ##############################################
 # Infer on final test set
 ##############################################
-# Define variables
-PWD = os.getcwd()
-dir_name = PWD + '/models/'
 experiment_name = '11_3_3'
 
-# Call the function
-train_load_transformer_model(dir_name = dir_name,
-          experiment_name = experiment_name,
+train_load_transformer_model(
+          dir_name = str(model_data_path) + pathlib.os.sep, # on MAC-OS: pathlib.os.sep = '/'
+          experiment_name = '11_3_3',
           df = df[:args.limit],
           anomalies = Anomaly_RootLabels_columns, 
           train_mode = False, 
